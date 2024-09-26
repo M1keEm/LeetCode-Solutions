88a3fdeec57ac938e7b6e1f96202da1bffdee611
Michał Banaszek <115193437+M1keEm@users.noreply.github.com>
2024-09-26 23:56:45 +0200

    MyCalendarI

---
+++ MyCalendarI/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    class MyCalendar {
+        TreeMap<Integer, Integer> calendar = new TreeMap<>();
+
+        public MyCalendar() {
+            calendar.put(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        }
+
+        public boolean book(int start, int end) {
+            Map.Entry<Integer, Integer> pair = calendar.higherEntry(start);
+            boolean res = end <= pair.getValue();
+            if (res)
+                calendar.put(end, start);
+            return res;
+        }
+    }
+}
