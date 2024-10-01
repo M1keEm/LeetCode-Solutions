8f3797ab2a8ef08fd76dc66e902bbd28c05cde1b
Michał Banaszek <115193437+M1keEm@users.noreply.github.com>
2024-10-01 17:45:45 +0200

    added dfs recursive function

--- LinkedListInBinaryTree/src/Main.java
+++ LinkedListInBinaryTree/src/Main.java
@@ -12,5 +12,12 @@ public class Main {
         if(dfs(node, head)) return true;
         return checkPath(node.left, head) || checkPath(node.right, head);
     }
+
+    private boolean dfs(TreeNode node, ListNode head) {
+        if(head == null) return true; //if head has ended, then we have matched all nodes in head with our tree
+        if(node == null) return false; //if tree has ended we haven't found head in our tree, not yet anyway
+        if(node.val != head.val) return false; //wrong values, do not match
+        return dfs(node.left, head.next) || dfs(node.right, head.next); //if the values match and neither node nor head has ended, check the children of the current node with head.next node
+    }
 }
 
