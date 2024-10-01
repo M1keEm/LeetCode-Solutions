4d0c95ac673f5c839e231c9534c8bd6e36b617b3
Michał Banaszek <115193437+M1keEm@users.noreply.github.com>
2024-10-01 19:17:33 +0200

    array solution
    Most efficient array solution of the "Check if Array Pairs Are Divisable by k" problem.

--- CheckIfArrPairsDivByK/src/Main.java
+++ CheckIfArrPairsDivByK/src/Main.java
@@ -1,27 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 
     public boolean canArrange(int[] arr, int k) {
-        Map<Integer, Integer> remainderCount = new HashMap<>();
+        int[] freq = new int[k];
         for (int i : arr) {
             int rem = ((i % k) + k) % k;
-            remainderCount.put(rem, remainderCount.getOrDefault(rem, 0) + 1);
+            freq[rem]++;
         }
-        for (int i : arr) {
-            int rem = ((i % k) + k) % k;
-            if (rem == 0) {
-                if (remainderCount.get(rem) % 2 == 1) {   //if the remainder of an element is even, the number of such elements mush be even
-                    return false;
-                }
-            } else if (!Objects.equals(remainderCount.get(rem), remainderCount.get(k - rem))) {
-                return false;
-            }
+        if (freq[0] % 2 == 1) {
+            return false;
+        }
+        for (int i = 1; i <= k / 2; i++) {
+            if (freq[i] != freq[k - i]) return false;
         }
         return true;
     }
