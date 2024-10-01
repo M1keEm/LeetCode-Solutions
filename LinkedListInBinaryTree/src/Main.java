ce6e13a42de59dd5d3e588c4df0b045fcddb13ab
Michał Banaszek <115193437+M1keEm@users.noreply.github.com>
2024-10-01 17:18:23 +0200

    isSubPath and checkPath recursive functions
    dfs left to complete recursive viability of the programme

--- LinkedListInBinaryTree/src/Main.java
+++ LinkedListInBinaryTree/src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    }
+
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(root == null) return false;
+        return checkPath(root, head);
+    }
+
+    private boolean checkPath(TreeNode node, ListNode head) {
+        if(node == null) return false;
+        if(dfs(node, head)) return true;
+        return checkPath(node.left, head) || checkPath(node.right, head);
     }
 }
 
