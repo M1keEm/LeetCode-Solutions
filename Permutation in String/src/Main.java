6f2587f95972785a730965045d34391e3bc391d5
Michał Banaszek <115193437+M1keEm@users.noreply.github.com>
2024-10-06 14:42:27 +0200

    array solution
    Much faster solution using int arrays[26] for storing letters frequencies instead of more complex data structures like hashmap or sorting - which is much slower.

--- Permutation in String/src/Main.java
+++ Permutation in String/src/Main.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
@@ -13,27 +10,27 @@ public class Main {
         if (s1.length() > s2.length()) {
             return false;
         }
-        HashMap<Character, Integer> s1map = new HashMap<>();
+        int[] s1Letters = new int[26];
 
-        for (int i = 0; i < s1.length(); i++){
-            s1map.put(s1.charAt(i), s1map.getOrDefault(s1.charAt(i), 0) + 1);
+        for (int i = 0; i < s1.length(); i++) {
+            s1Letters[s1.charAt(i) - 97]++;
         }
 
         for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            HashMap<Character, Integer> s2map = new HashMap<>();
+            int[] s2Letters = new int[26];
             for (int j = 0; j < s1.length(); j++) {
-                s2map.put(s2.charAt(i + j), s2map.getOrDefault(s2.charAt(i + j), 0) + 1);
+                s2Letters[s2.charAt(i + j) - 97]++;
             }
-            if (matches(s1map, s2map)){
+            if (matches(s1Letters, s2Letters)) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean matches(HashMap<Character, Integer> s1map, HashMap<Character, Integer> s2map) {
-        for (char key : s1map.keySet()) {
-            if (s1map.get(key) - s2map.getOrDefault(key, -1) != 0){
+    public boolean matches(int[] arr1, int[] arr2) {
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
                 return false;
             }
         }
