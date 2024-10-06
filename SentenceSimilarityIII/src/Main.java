2bb59e405f30933d03f653a29d39d1b92398ef1d
Michał Banaszek <115193437+M1keEm@users.noreply.github.com>
2024-10-07 00:26:51 +0200

    Simplest and fastest solution

--- SentenceSimilarityIII/src/Main.java
+++ SentenceSimilarityIII/src/Main.java
@@ -1,7 +1,3 @@
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
@@ -10,28 +6,23 @@ public class Main {
         System.out.println(main.areSentencesSimilar(sentence1, sentence2));
     }
 
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        String[] words1 = sentence1.split(" ");
+        String[] words2 = sentence2.split(" ");
+        int start = 0;
+        int end1 = words1.length-1;
+        int end2 = words2.length-1;
+        if(words1.length > words2.length){
+            return areSentencesSimilar(sentence2, sentence1);
+        }
 
-    public boolean areSentencesSimilar(String s1, String s2) {
-        Deque<String> deque1 = new ArrayDeque<>(Arrays.asList(s1.split(" ")));
-        Deque<String> deque2 = new ArrayDeque<>(Arrays.asList(s2.split(" ")));
-        //Compare the prefixes or beginning of the strings.
-        while (
-                !deque1.isEmpty() &&
-                        !deque2.isEmpty() &&
-                        deque1.peek().equals(deque2.peek())
-        ) {
-            deque1.poll();
-            deque2.poll();
+        while(start < words1.length && words1[start].equals(words2[start])) {
+            start++;
         }
-        // Compare the suffixes or ending of the strings.
-        while (
-                !deque1.isEmpty() &&
-                        !deque2.isEmpty() &&
-                        deque1.peekLast().equals(deque2.peekLast())
-        ) {
-            deque1.pollLast();
-            deque2.pollLast();
+        while (end1 >= 0 && words1[end1].equals(words2[end2])) {
+            end1--;
+            end2--;
         }
-        return deque1.isEmpty() || deque2.isEmpty();
+        return end1 < start;
     }
 }
