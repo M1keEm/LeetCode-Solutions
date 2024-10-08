314c311148ae3c5601abad3dcedd4d950444f942
Michał Banaszek <115193437+M1keEm@users.noreply.github.com>
2024-10-08 23:33:53 +0200

    Min Number of Swaps to Make the String Balanced
    stack solution

---
+++ MinNumberofSwapsToMakeTheStringBalanced/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+
+    public int minSwaps(String s) {
+        Stack<Character> stack = new Stack<>();
+        int unbalanced = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            // If an opening bracket is encountered, push it in the stack.
+            if (ch == '[') stack.push(ch);
+            else {
+                // If the stack is not empty, pop it.
+                if (!stack.isEmpty()) stack.pop();
+                    // Otherwise increase the count of unbalanced brackets.
+                else unbalanced++;
+            }
+        }
+        return (unbalanced + 1) / 2;
+    }
+}
+}
+
