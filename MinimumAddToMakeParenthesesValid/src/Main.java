fadc0409288d7895aa31b6a599d4585e9c073387
Michał Banaszek <115193437+M1keEm@users.noreply.github.com>
2024-10-09 23:47:52 +0200

    Minimum Add to Make Parentheses Valid
    open bracket counter solution

---
+++ MinimumAddToMakeParenthesesValid/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    class Solution {
+
+        public int minAddToMakeValid(String s) {
+            int openBrackets = 0;
+            int minAddsRequired = 0;
+
+            for (char c : s.toCharArray()) {
+                if (c == '(') {
+                    openBrackets++;
+                } else {
+                    // If an open bracket exists, match it with the closing one
+                    // If not, we need to add an open bracket.
+                    if (openBrackets > 0) {
+                        openBrackets--;
+                    } else {
+                        minAddsRequired++;
+                    }
+                }
+            }
+
+            // Add the remaining open brackets as closing brackets would be required.
+            return minAddsRequired + openBrackets;
+        }
+    }
+}
