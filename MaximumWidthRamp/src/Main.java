82380ad642a9236456dcd3aff681d604ab7d8254
Michał Banaszek <115193437+M1keEm@users.noreply.github.com>
2024-10-10 23:13:38 +0200

    custom sorting solution
    more efficient than the O(n^2) time complexity brute force solution

--- MaximumWidthRamp/src/Main.java
+++ MaximumWidthRamp/src/Main.java
@@ -3,22 +3,25 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
-        int [] nums = {6,0,8,2,1,5};
+        int[] nums = {6, 0, 8, 2, 1, 5};
         System.out.println(main.maxWidthRamp(nums));
 
     }
 
-    public int maxWidthRamp(int [] nums){
+    public int maxWidthRamp(int[] nums) {
         int n = nums.length;
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+        Arrays.sort(indices, (a, b) ->
+            nums[a] == nums[b] ? a - b : nums[a] - nums[b]); //if the values are the same, sort by index, otherwise sort by value of num[index]
         int maxWidth = 0;
-        for(int i = 0; i<n; ++i){
-            for(int j = i + 1; j<n; ++j){
-                if(nums[i] <= nums[j]){
-                    maxWidth = Math.max(maxWidth, j-i);
-                }
-            }
+        int minIndex = n;
+        for (int index : indices) {
+            maxWidth = Math.max(maxWidth, index - minIndex);
+            minIndex = Math.min(minIndex, index);
         }
         return maxWidth;
-
     }
 }
