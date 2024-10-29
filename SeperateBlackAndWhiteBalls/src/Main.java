785ea8bc33a249c48b1a1c2a2b4d8a5008dd8ffa
Michał Banaszek <115193437+M1keEm@users.noreply.github.com>
2024-10-29 17:27:28 +0100

    remove redundant long casting

--- SeperateBlackAndWhiteBalls/src/Main.java
+++ SeperateBlackAndWhiteBalls/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
             for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == '0') {
                     // Swap wti all black balls to the left
-                    countSwaps += (long) blackBallCount;
+                    countSwaps += blackBallCount;
                 } else {
                     //increment black ball count
                     blackBallCount++;
