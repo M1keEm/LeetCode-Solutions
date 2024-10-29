5a4d6bf11b91b78feefeb276ea4baee7246a1776
Michał Banaszek <115193437+M1keEm@users.noreply.github.com>
2024-10-29 17:11:49 +0100

    string.charAt(i) instead of arr[i]

--- SeperateBlackAndWhiteBalls/src/Main.java
+++ SeperateBlackAndWhiteBalls/src/Main.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.minimumSteps("00011000111"));
+        System.out.println(sol.minimumSteps("000110001011010101011111110000000000000000111"));
     }
 
     static class Solution {
         public long minimumSteps(String s) {
             long countSwaps = 0;
             long firstSpaceForWhite = 0;
-            char[] arr = s.toCharArray();
-            for (int currentPosition = 0; currentPosition < arr.length; currentPosition++) {
-                if (arr[currentPosition] == '0') {
+            for (int currentPosition = 0; currentPosition < s.length(); currentPosition++) {
+                if (s.charAt(currentPosition) == '0') {
                     countSwaps += currentPosition - firstSpaceForWhite;
                     firstSpaceForWhite++;
                 }
